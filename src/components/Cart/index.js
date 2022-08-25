3c4064a6cb24c149a95f312ddb1f447dd310c5cd
hipnos1606 <joannmurillo81@gmail.com>
2022-08-25 10:21:45 -0600

    refactored to have access to cart Sale Reference

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
 import {
   Switch,
   Text,
@@ -9,37 +9,16 @@ import {
   FlatList
 } from 'react-native';
 import store from '../../../store';
-import {getTotal, moneyFormat, postSale} from '../mainFunctions';
 import {interstitialAdConfig, InterstitialUnitId} from '../ads';
 import {AdEventType, InterstitialAd} from '@react-native-firebase/admob';
-import {clearStoreCart} from './cartComponents/functions';
 import ProductsList from './cartComponents/productsList';
 import ServicesList from './cartComponents/servicesList';
 import styles from './styles';
 import EmptyListImage from '../emptyListImage';
 
 const Cart = () => {
-  const storeState = store.getState();
-  const [state, setState] = useState({
-    products: storeState.cart.products,
-    services: storeState.cart.services,
-    client: storeState.cart.client,
-  });
-  const [saleState, setSaleState] = useState(storeState.data.defaultSaleState);
   
-  const stateChange = (prevState, newState) => {
-    newState = {
-      products: newState.cart.products,
-      services: newState.cart.services,
-      client: newState.cart.client,
-    };
-    
-    const prevPlainState = JSON.stringify(prevState);
-    const newPlainState = JSON.stringify(newState);
-    
-    return prevPlainState !== newPlainState ? true : false;
-  };
-  
+  const cart = store.getState().cart;
   useEffect(() => {
     const interstitial = InterstitialAd.createForAdRequest(InterstitialUnitId, interstitialAdConfig);
     interstitial.load();
@@ -50,80 +29,48 @@ const Cart = () => {
       }
     });
 
-    const unsubscribe = store.subscribe(() => {
-      const prevState = state;
-      const newState = store.getState();
-      const theStateChange = stateChange(prevState, newState);
-      if (theStateChange) {
-        setState({
-          products: newState.cart.products,
-          services: newState.cart.services,
-          client: newState.cart.client,
-        });
-      }
-    });
-
     return () => {
-      unsubscribe();
       adEventListener();
     };
   }, []);
-
-  const servicesList = state.services ? state.services : []
-  const productsList = state.products ? state.products : []
-  const total = getTotal([productsList, servicesList], state.wholesaler);
-  return state.products.length === 0 && servicesList.length === 0 ? (
+  return cart.productos.length === 0 && cart.servicios.length === 0 ? (
     <View style={{alignItems: 'center', justifyContent: 'center', flex: 1, backgroundColor: 'white'}}>
-      {EmptyListImage.default}
-      <Text style={{textAlign: 'center', color: 'gray'}}>Seleccione productos en la sección "Nueva Venta" para visualizarlos aquí</Text>
+      {EmptyListImage.ImageComponent('Seleccione productos en la sección "Nueva Venta" para visualizarlos aquí')}
     </View>
   ) : (
     <View style={styles.container}>
       <FlatList
-        data={[productsList, servicesList]}
-        ListHeaderComponent={() => (<ProductsList products={productsList} wholesaler={state.wholesaler} />)}
-        ListFooterComponent={() => (<ServicesList services={servicesList} wholesaler={state.wholesaler} />)}
+        data={[cart.productos, cart.servicios]}
+        ListHeaderComponent={() => (<ProductsList products={cart.productos} />)}
+        ListFooterComponent={() => (<ServicesList services={cart.servicios} />)}
        />
       <View style={styles.bottomView}>
         <View style={styles.bottomViewContent}>
-          {state.client ? (
+          {cart.client ? (
             <Text style={styles.client}>
-              {'Cliente: ' + state.client.nombre}
-            </Text>
-          ) : null}
-          {state.wholesaler ? (
-            <Text style={styles.client}>
-              {'Comprador mayorista: ' + state.wholesaler.nombre}
+              {'Cliente: ' + cart.client.nombre}
             </Text>
           ) : null}
           <View style={styles.bottomViewContentRow}>
             <View style={styles.saleStateContainer}>
               <Text style={styles.saleStateText}>
-                {saleState ? 'Vendida' : 'Pendiente'}
+                {cart.estado ? 'Vendida' : 'Pendiente'}
               </Text>
               <Switch
                 style={styles.switchSaleState}
                 trackColor={{false: '#e6e8f1', true: '#e6e8f1'}}
-                thumbColor={saleState ? '#434588' : '#b4b6be'}
+                thumbColor={cart.estado ? '#434588' : '#b4b6be'}
                 onValueChange={() => {
-                  setSaleState(!saleState);
+                  cart.updateProperty('estado', !cart.estado);
                 }}
-                value={saleState}
+                value={cart.estado}
               />
             </View>
           </View>
         </View>
         <TouchableOpacity
           onPress={() => {
-            postSale({
-              ...state,
-              total: getTotal(
-                [productsList, servicesList],
-                state.wholesaler,
-              ),
-              saleState,
-            }).then(() => {
-              clearStoreCart();
+            cart.postSale().then(() => {
               ToastAndroid.show(
                 'La venta se realizó correctamente',
                 ToastAndroid.SHORT,
@@ -133,19 +80,10 @@ const Cart = () => {
           style={styles.soldButton}>
             <Text style={styles.soldButtonText}>Realizar venta</Text>
             <Text style={{...styles.soldButtonText, textAlign: 'right'}}>
-              {moneyFormat(total)}
+              {cart.total}
             </Text>
         </TouchableOpacity>
       </View>
-    </View>
-  );
-};
-
-const Subtotal = ({text, value}) => {
-  return (
-    <View style={styles.subtotal}>
-      <Text>{text} </Text>
-      <Text>{moneyFormat(value)}</Text>
     </View>
   );
 };
