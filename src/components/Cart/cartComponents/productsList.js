7e0665d67c423e60a1c020c8ed7b18eeab60a146
hipnos1606 <joannmurillo81@gmail.com>
2022-08-25 10:16:46 -0600

    removed unnecesary code lines

--- src/components/Cart/cartComponents/productsList.js
+++ src/components/Cart/cartComponents/productsList.js
@@ -1,20 +1,17 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, FlatList} from 'react-native';
-import {RenderImage} from '../../auxComponents';
+import {View, Text, FlatList, Image} from 'react-native';
 import QuantityEditor from './quantityEditor';
-import {moneyFormat} from '../../mainFunctions';
 import styles from './styles';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
-import { removeProductFromCart } from './functions';
 
+const ProductsList = ({products}) => {
 
-const ProductsList = ({products, wholesaler}) => {
   return (
     <FlatList 
       data={products}
       keyExtractor={item => item.id}
-      renderItem={({item}) => <ListItem data={item} wholesaler={wholesaler} />}
+      renderItem={({item}) => <ListItem data={item} />}
       style={styles.cartList}
       ItemSeparatorComponent={() => <View style={styles.separator} />}
       showsVerticalScrollIndicator={false}
@@ -34,48 +31,42 @@ const RightSwipeAction = () => (
   </View>
 )
 
-const ListItem = ({data, wholesaler}) => <Swipeable
-  renderLeftActions={LeftSwipeAction} 
-  renderRightActions={RightSwipeAction} 
-  onSwipeableLeftOpen={() => {removeProductFromCart(data.id)}}
-  onSwipeableRightOpen={() => {removeProductFromCart(data.id)}}  
-  >
-    <Product data={data} wholesaler={wholesaler} />
-  </Swipeable>
+const ListItem = ({data, wholesaler}) => {
+  const cart = {
+    products:[],
+    services: [],
+  };
+  return <Swipeable
+    renderLeftActions={LeftSwipeAction} 
+    renderRightActions={RightSwipeAction} 
+    onSwipeableLeftOpen={() => {cart.removeTo('productos', data.id)}}
+    onSwipeableRightOpen={() => {cart.removeTo('servicios', data.id)}}  
+    >
+      <Product data={data} key={data.id} />
+    </Swipeable>
+}
 
 
-function Product ({data, wholesaler}) {
-  const {
-    imageURL,
-    nombre,
-    marca,
-    descripcion,
-    precioVenta,
-    precioMayoreo,
-    cantidad,
-    id,
-  } = data;
-
+const Product = ({data}) => {
   return (
       <View style={styles.productContainer}>
-        <RenderImage
-          source={{uri: imageURL}}
-          priority="high"
+        <Image
+          source={{uri: data.imageURL}}
           style={styles.productImage}
         />
         <View style={styles.productDescriptionContainer}>
-          <Text style={styles.productName} ellipsizeMode="tail" numberOfLines={2}>{nombre}</Text>
+          <Text style={styles.productName} ellipsizeMode="tail" numberOfLines={2}>{data.nombre}</Text>
           {
-            descripcion ? <Text style={styles.productDescription}>{descripcion}</Text> : null
+            data.descripcion ? <Text style={styles.productDescription}>{data.descripcion}</Text> : null
           }
           {
-            marca ? <Text style={styles.productBrand}>{marca}</Text> : null
+            data.marca ? <Text style={styles.productBrand}>{data.marca}</Text> : null
           }
           <View style={styles.priceContainer}>
             <Text style={styles.productPrice}>
-              {wholesaler ? moneyFormat(precioMayoreo) : moneyFormat(precioVenta)}
+              {data.getPrecioVenta()}
             </Text>
-            <QuantityEditor defaultQuantity={cantidad} type="PRODUCT" id={id} />
+            <QuantityEditor defaultQuantity={data.cantidad} type="PRODUCT" id={data.id} />
           </View>
         </View>
     </View>
